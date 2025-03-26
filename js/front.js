4e3797af33ee5d2bb278823b4fe25bb14d5c2f43
Guido <guidotoritto@outlook.com>
2025-03-26 00:39:28 -0300

    10 comit

--- js/front.js
+++ js/front.js
@@ -124,10 +124,4 @@ document.onpointerdown = function (e) {
   return false;
 };
 
-document.onmousewheel = function(e) {
-  e = e || window.event;
-  var d = e.wheelDelta / 20 || -e.detail;
-  radius += d;
-  init(1);
-};
 
